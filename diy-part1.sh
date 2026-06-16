03b1945a8820bf0ab88d76353da774690310e3fe
TimLangers <yangcui2049@163.com>
2026-06-16 08:46:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-# 添加 PassWall 源（使用 Openwrt-Passwall 组织的新仓库）
-# 注意：按照官方说明，需要同时添加 packages 和 luci 两个源
+# 核心源：PassWall
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
 
-# 添加 OpenClash 源
+# 核心源：OpenClash
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 
-# 添加 Argon 主题源（确保最新）
+# 核心源：Argon 主题与配置
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
 echo 'src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config.git' >> feeds.conf.default
