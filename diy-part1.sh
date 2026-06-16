e92645a308a50300676e608a712379bec7549ba4
TimLangers <yangcui2049@163.com>
2026-06-16 22:26:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,11 +2,9 @@
 
 export GIT_SSL_NO_VERIFY=true
 
-# OpenClash
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 
-# Argon
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
 echo 'src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config.git' >> feeds.conf.default
 
-echo "feed source added."
+echo "feeds added"
