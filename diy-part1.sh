f6842964157df2181cde975daff723ed84b7040e
TimLangers <yangcui2049@163.com>
2026-06-16 23:41:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# 1. 强力清理：删除可能干扰 feeds 的旧文件夹（防止之前 git clone 的残留干扰）
-rm -rf package/luci-app-openclash
-rm -rf package/luci-theme-argon
-rm -rf package/luci-app-argon-config
+# 1. 强制清理：先移除所有已有的同名 feed 定义，防止重复
+sed -i '/packages/d' feeds.conf.default
+sed -i '/routing/d' feeds.conf.default
+sed -i '/telephony/d' feeds.conf.default
+sed -i '/luci/d' feeds.conf.default
+sed -i '/openclash/d' feeds.conf.default
+sed -i '/argon/d' feeds.conf.default
 
-# 2. 确保 feeds.conf.default 干净（可选：如果你想每次都重置）
-# 如果你不想每次都重置，这行可以注释掉
-# cp feeds.conf.default feeds.conf.default.bak
+# 2. 重新添加基础 feeds
+echo "src-git packages https://github.com/openwrt/packages.git" >> feeds.conf.default
+echo "src-git routing https://git.openwrt.org/feed/routing.git" >> feeds.conf.default
+echo "src-git telephony https://git.openwrt.org/feed/telephony.git" >> feeds.conf.default
+echo "src-git luci https://github.com/openwrt/luci.git" >> feeds.conf.default
 
-# 3. 添加基础 feeds (防止重复添加，加了简单的检查)
-grep -q "github.com/openwrt/packages.git" feeds.conf.default || sed -i '$a src-git packages https://github.com/openwrt/packages.git' feeds.conf.default
-grep -q "feed/routing.git" feeds.conf.default || sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
-grep -q "feed/telephony.git" feeds.conf.default || sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
-grep -q "github.com/openwrt/luci.git" feeds.conf.default || sed -i '$a src-git luci https://github.com/openwrt/luci.git' feeds.conf.default
-
-# 4. 添加插件 feeds
-grep -q "OpenClash" feeds.conf.default || sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-grep -q "luci-theme-argon" feeds.conf.default || sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
-grep -q "luci-app-argon-config" feeds.conf.default || sed -i '$a src-git argon_config https://github.com/jerrykuku/luci-app-argon-config.git' feeds.conf.default
+# 3. 添加自定义插件 feeds
+echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
+echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git" >> feeds.conf.default
+echo "src-git argon_config https://github.com/jerrykuku/luci-app-argon-config.git" >> feeds.conf.default
 
 echo "==== diy-part1.sh finished successfully ===="
