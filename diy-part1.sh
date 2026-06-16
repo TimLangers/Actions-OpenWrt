89418bdddbef000a9d72d02b26ac891b6fbf548d
TimLangers <yangcui2049@163.com>
2026-06-16 23:35:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export GIT_SSL_NO_VERIFY=true
+# 1. 确保基础 feeds 存在 (必须)
+sed -i '$a src-git packages https://github.com/openwrt/packages.git' feeds.conf.default
+sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
+sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
+sed -i '$a src-git luci https://github.com/openwrt/luci.git' feeds.conf.default
 
-echo "==== cloning packages (clean mode) ===="
+# 2. 以 feed 形式添加 OpenClash
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
-# OpenClash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+# 3. 如果是 Argon，建议放在 luci 目录下或者通过 feed 添加
+# 示例：通过 feed 添加 (前提是该仓库支持)
+sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
 
-# Argon 主题
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-# Argon 配置
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-echo "==== diy-part1 done ===="
+echo "==== diy-part1.sh finished successfully ===="
