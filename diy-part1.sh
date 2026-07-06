3cba9291d362590c69cb8b9a7e477f95d7d74951
TimLangers <yangcui2049@163.com>
2026-07-07 07:31:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,11 +9,11 @@ echo "src-git routing https://git.openwrt.org/feed/routing.git;openwrt-25.12" >>
 echo "src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-25.12" >> feeds.conf.default
 echo "src-git luci https://github.com/openwrt/luci.git;openwrt-25.12" >> feeds.conf.default
 
-# 3. 添加 OpenClash 源
-echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
+# 3. 添加 OpenClash 源（使用 master 分支）
+echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
 
-# 4. 添加 Argon 主题及其配置插件
-echo "src-git luci_theme_argon https://github.com/jerrykuku/luci-theme-argon.git" >> feeds.conf.default
-echo "src-git luci_app_argon_config https://github.com/jerrykuku/luci-app-argon-config.git" >> feeds.conf.default
+# 4. 添加 Argon 主题及其配置插件（使用 master 分支确保兼容 25.12 架构）
+echo "src-git luci_theme_argon https://github.com/jerrykuku/luci-theme-argon.git;master" >> feeds.conf.default
+echo "src-git luci_app_argon_config https://github.com/jerrykuku/luci-app-argon-config.git;master" >> feeds.conf.default
 
 echo "==== diy-part1.sh finished successfully ===="
